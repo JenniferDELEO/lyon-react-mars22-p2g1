6d27d06ef616b0861c7c2ece0806a4485ee4f8a1
Jennifer DE LEO <deleo.jennifer1@gmail.com>
2022-04-12 22:39:59 +0200

    created the 2 popup homepage

---
+++ src/components/Popup.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Popup.css';
+
+function Popup({ trigger, children, setTrigger }) {
+  return trigger ? (
+    <div className="popup">
+      <div className="popup-inner">
+        <button
+          type="button"
+          className="close-btn"
+          onClick={() => setTrigger(false)}
+        >
+          X
+        </button>
+        {children}
+      </div>
+    </div>
+  ) : (
+    ''
+  );
+}
+
+export default Popup;
