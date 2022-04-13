c1bb68f2bd9ee11347a741c14c05d92f864bc414
PIERRE DARTEVELLE <pdarteve5@gmail.com>
2022-04-13 22:14:17 +0200

    fixed component with the new json files

--- src/components/HomeBoxList.jsx
+++ src/components/HomeBoxList.jsx
@@ -1,15 +1,15 @@
 /* eslint-disable react/no-array-index-key */
 // import React, { useState } from 'react';
-import boxData from '../ressources/coordsBAL.json';
-import bookData from '../ressources/livresDB.json';
+import boxList from '../ressources/coordsBAL.json';
+import bookList from '../ressources/livresDB.json';
 
 const quantiteParBoite = [];
-for (let i = 0; i < boxData.length; i += 1) {
+
+for (let i = 0; i < boxList.length; i += 1) {
   quantiteParBoite.push(0);
 }
-
-for (let i = 0; i < bookData.length; i += 1) {
-  const index = parseFloat(bookData[i].boite);
+for (let i = 0; i < bookList.length; i += 1) {
+  const index = parseFloat(bookList[i].boite);
   quantiteParBoite[index] += 1;
 }
 function HomeBoxList() {
@@ -17,9 +17,9 @@ function HomeBoxList() {
     <>
       <h2>Liste des boîtes :</h2>
       <ul>
-        {boxData.map((box, i) => (
-          <li key={i}>{box.adresse }  boîte numéro : {box.num}
-            <br />Quantité de livres : {quantiteParBoite[box.num]}
+        {boxList.map((box, i) => (
+          <li key={i}>{box.adresse }  boîte numéro : {box.boite}
+            <br /> Quantité de livres : {quantiteParBoite[box.boite]}
           </li>
         ))}
       </ul>
