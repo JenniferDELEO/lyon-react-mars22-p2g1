0244ed2214bbee314cf9ccb571562e11bbe7c67f
PIERRE DARTEVELLE <pdarteve5@gmail.com>
2022-04-13 08:21:08 +0200

    ossature liste des boites page d'accueil

---
+++ src/components/HomeBoxList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function HomeBoxList() {
+  return (
+    <>
+      {/* h1 réservé pour le main titre? */}
+      <h2>Liste des boîtes :</h2>
+      <ul>
+        <li>{/* randomBox.preview */}</li>
+      </ul>
+
+    </>
+  );
+}
+
+export default HomeBoxList;
