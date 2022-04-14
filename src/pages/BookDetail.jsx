fbb969c80b268443f277dc30e7fe55adf4ba09e0
Gaspard Mazataud <gaspard.mzt@gmail.com>
2022-04-14 16:21:38 +0200

    bookdetail without style from a given book

--- src/pages/BookDetail.jsx
+++ src/pages/BookDetail.jsx
@@ -1,5 +1,42 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 
 export default function BookDetail() {
-  return <div>Bookdetail page</div>;
+  const entree = 9781781101063;
+  const [book, setBook] = useState();
+  React.useEffect(() => {
+    axios
+      .get(`https://www.googleapis.com/books/v1/volumes?q=isbn:${entree}`)
+      .then((answer) => answer.data.items)
+      .then((bookInfo) => {
+        setBook(bookInfo[0].volumeInfo);
+      });
+  }, []);
+
+  return (
+    <div className="Bookdetail">
+      {book && (
+        <div>
+          <h1>{book.title}</h1>
+          <p>Auteur : {book.authors[0]}</p>
+          <p>Date de publication : {book.publishedDate.slice(0, 4)}</p>
+          <p>{book.pageCount} pages</p>
+          <img src={book.imageLinks.thumbnail} alt={book.title} />
+          <p>
+            <strong>Résumé :</strong> {book.description}
+          </p>
+          <p>ISBN (13 chiffres): {book.industryIdentifiers[1].identifier}</p>
+        </div>
+      )}
+    </div>
+  );
 }
+
+/*  const ISBN13fromAPI = null;
+   for (let i = 0; i < book.industryIdentifiers.length; i++) {
+     if (book.industryIdentifiers[i].type === 'ISBN_13') {
+      ISBN13fromAPI = book.industryIdentifiers[i].identifier;
+     }
+    console.log(ISBN13fromAPI);
+   }
+*/
