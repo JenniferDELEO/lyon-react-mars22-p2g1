63e02db6b62de7f7cae71b368746730680ef3487
CLément <clmentmoreno@hotmail.com>
2022-04-14 15:55:19 +0200

    Solved the map issue

--- src/components/Mapmodule.jsx
+++ src/components/Mapmodule.jsx
@@ -7,7 +7,7 @@ const DEFAULT_LANGITUDE = -123;
 function Map() {
   return (
     <div className="map">
-      <LeafletMap center={[DEFAULT_LATITUDE, DEFAULT_LANGITUDE]}>
+      <LeafletMap center={[DEFAULT_LATITUDE, DEFAULT_LANGITUDE]} zoom={13}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
