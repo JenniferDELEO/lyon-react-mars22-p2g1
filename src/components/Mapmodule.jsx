f6217d1d369a57c632b864a76a367371b9c56695
CLément <clmentmoreno@hotmail.com>
2022-04-15 15:09:27 +0200

    erase dead code

--- src/components/Mapmodule.jsx
+++ src/components/Mapmodule.jsx
@@ -1,26 +1,16 @@
-/* import React from 'react';
-import { MapContainer as LeafletMap, Marker, TileLayer } from 'react-leaflet';
+import React from 'react';
+import { MapContainer as LeafletMap, TileLayer } from 'react-leaflet';
 import { geolocated } from 'react-geolocated';
 
-const DEFAULT_LATITUDE = 45.764043;
-const DEFAULT_LANGITUDE = 4.835659;
-
 function Map() {
- const latitude = this.props.coords
-    ? this.props.coords.latitude
-    : DEFAULT_LANGITUDE;
-  const longitude = this.props.coords
-    ? this.props.coords.longitude
-    : DEFAULT_LATITUDE;
   return (
     <div className="map">
-      <LeafletMap center={[latitude, longitude]} zoom={18}>
+      <LeafletMap center={[]} zoom={18}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
       </LeafletMap>
-      <Marker position={[latitude, longitude]}></Marker>
     </div>
   );
 }
@@ -30,4 +20,4 @@ export default geolocated({
     enableHighAccuracy: false,
   },
   userDecisionTimeout: 10000,
-})(Map); */
+})(Map);
