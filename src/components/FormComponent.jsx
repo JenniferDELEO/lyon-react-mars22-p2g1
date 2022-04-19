74a49ee2e0e3802de157c6ddf0ff81eb40c8d918
Jennifer DE LEO <deleo.jennifer1@gmail.com>
2022-04-19 17:31:43 +0200

    finished configuration of emailjs

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -1,17 +1,40 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import '../styles/FormComponent.css';
+import emailjs from '@emailjs/browser';
 
 function FormComponent() {
+  const form = useRef();
   const [name, setName] = useState('');
   const [mail, setMail] = useState('');
   const [message, setMessage] = useState('');
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        'service_4if9eu4',
+        'template_xmbjes5',
+        form.current,
+        '0TYQA1GenX3etokSV'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    alert('Merci pour votre message');
+  };
   return (
     <div>
       <h2 className="form-title">Votre message :</h2>
-      <form className="form-component">
+      <form ref={form} className="form-component" onSubmit={sendEmail}>
         <label htmlFor="name">Votre nom * :</label>
         <input
           type="text"
+          name="name"
           value={name}
           onChange={(event) => setName(event.target.value)}
           required
@@ -19,6 +42,7 @@ function FormComponent() {
         <label htmlFor="mail">Votre mail * :</label>
         <input
           type="text"
+          name="mail"
           value={mail}
           onChange={(event) => setMail(event.target.value)}
           required
@@ -32,10 +56,10 @@ function FormComponent() {
           onChange={(event) => setMessage(event.target.value)}
           required
         />
+        <button type="submit" className="btn-form" onClick={sendEmail}>
+          Envoyer
+        </button>
       </form>
-      <button type="submit" className="btn-form">
-        Envoyer
-      </button>
     </div>
   );
 }
