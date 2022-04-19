68f4bd8d7031c11d4caebe7e3270cb1dd76676c9
CLément <clmentmoreno@hotmail.com>
2022-04-19 14:17:37 +0200

    error console solved

--- src/components/Mapmodule.jsx
+++ src/components/Mapmodule.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { MapContainer as LeafletMap, TileLayer } from 'react-leaflet';
-import { geolocated } from 'react-geolocated';
+
+const latitude = 45.764043;
+const longitude = 4.835659;
 
 function Map() {
   return (
     <div className="map">
-      <LeafletMap center={[]} zoom={18}>
+      <LeafletMap center={[latitude, longitude]} zoom={18}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -15,9 +17,4 @@ function Map() {
   );
 }
 
-export default geolocated({
-  positionOptions: {
-    enableHighAccuracy: false,
-  },
-  userDecisionTimeout: 10000,
-})(Map);
+export default Map;
