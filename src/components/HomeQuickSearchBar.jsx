9d66471bfc2a8cccf79d512eef412ed34f0e2f76
Gaspard Mazataud <gaspard.mzt@gmail.com>
2022-04-20 17:06:05 +0200

    working on it

--- src/components/HomeQuickSearchBar.jsx
+++ src/components/HomeQuickSearchBar.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import { React, useState } from 'react';
 import '../styles/HomeQuickSearchBar.css';
 
 export default function HomeQuickSearchBar() {
+  const [userQuickSearch, setuserQuickSearch] = useState('');
+  const handleQuickSearchChange = (e) => setuserQuickSearch(e.target.value);
+  const handleSubmit = () => {
+    alert('Lancement de la recherche rapide');
+  };
+
   return (
     <div className="HomeQuickSearchBar">
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           id="quickSearch"
           name="quickSearch"
           placeholder="Que voulez-vous lire aujourd'hui ?"
           required="required"
+          value={userQuickSearch}
+          onChange={handleQuickSearchChange}
         />
+        <div type="submit" className="buttForm" />
       </form>
     </div>
   );
