64b59147342af41cf6a8496c91e1577eba789810
Gaspard Mazataud <gaspard.mzt@gmail.com>
2022-04-20 15:20:29 +0200

    going on with style

--- src/components/HomeQuickSearchBar.jsx
+++ src/components/HomeQuickSearchBar.jsx
@@ -4,11 +4,15 @@ import '../styles/HomeQuickSearchBar.css';
 export default function HomeQuickSearchBar() {
   return (
     <div className="HomeQuickSearchBar">
-      <input
-        type="text"
-        placeholder="Que voulez-vous lire aujourd'hui ?"
-        required="required"
-      />
+      <form>
+        <input
+          type="text"
+          id="quickSearch"
+          name="quickSearch"
+          placeholder="Que voulez-vous lire aujourd'hui ?"
+          required="required"
+        />
+      </form>
     </div>
   );
 }
