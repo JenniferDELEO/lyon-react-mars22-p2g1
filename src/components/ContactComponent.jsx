096b72c9e2882c7edc6f0fcbad9c5e7ab65f81ec
PIERRE DARTEVELLE <pdarteve5@gmail.com>
2022-04-20 13:59:15 +0200

    adjusted avatar sizes and flexed for media queries

--- src/components/ContactComponent.jsx
+++ src/components/ContactComponent.jsx
@@ -4,24 +4,44 @@ import Woman from '../assets/woman.png';
 import Gaspard from '../assets/gaspard.png';
 import Clement from '../assets/clement.png';
 import Pierre from '../assets/pierre.png';
+import { Link } from 'react-router-dom';
+import '../styles/Avatars.css';
 
 function ContactComponent() {
   return (
     <div className="Avatars">
-      <div className="Jen">
-        <img src={Woman} alt="Jen" /> Jen
+      <div className="Jen avatar">
+        <Link to="//github.com/JenniferDELEO">
+          {' '}
+          <img src={Woman} alt="Jen" />{' '}
+        </Link>{' '}
+        Jennifer De Léo
       </div>
-      <div className="Adrien">
-        <img src={Adrien} alt="Adrien" /> Adrien
+      <div className="Adrien avatar">
+        <Link to="//github.com/01adrien">
+          {' '}
+          <img src={Adrien} alt="Adrien" />
+        </Link>{' '}
+        Adrien Gaucher
       </div>
-      <div className="Gaspard">
-        <img src={Gaspard} alt="Gaspard" /> Gaspard
+      <div className="Gaspard avatar">
+        <Link to="//github.com/GaspardMzt">
+          {' '}
+          <img src={Gaspard} alt="Gaspard" />{' '}
+        </Link>
+        Gaspard Mazataud
       </div>
-      <div className="Clement">
-        <img src={Clement} alt="Clement" /> Clement
+      <div className="Clement avatar">
+        <Link to="//github.com/ClemMoFr">
+          <img src={Clement} alt="Clement" />
+        </Link>{' '}
+        Clément Moreno
       </div>
-      <div className="Pierre">
-        <img src={Pierre} alt="Pierre" /> Pierre
+      <div className="Pierre avatar">
+        <Link to="//github.com/PiDartevelle">
+          <img src={Pierre} alt="Pierre" />
+        </Link>{' '}
+        Pierre Dartevelle
       </div>
     </div>
   );
