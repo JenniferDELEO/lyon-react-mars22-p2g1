115436234cd13c6e4acea27b6f3ecc91b7689ec4
PIERRE DARTEVELLE <pdarteve5@gmail.com>
2022-04-21 12:54:43 +0200

    resolved conflict, renamed to FormComponent

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable react/jsx-indent */
 import React from 'react';
 import FormComponent from '../components/FormComponent';
+import ContactComponent from '../components/ContactComponent';
 
 export default function Form() {
   return (
     <div>
       <FormComponent />
+      <ContactComponent />
     </div>
   );
 }
