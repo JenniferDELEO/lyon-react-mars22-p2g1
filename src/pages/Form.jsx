c9c7dde0388b40031fe7ce48c121235dc85909d0
PIERRE DARTEVELLE <pdarteve5@gmail.com>
2022-04-21 13:01:27 +0200

    add ContactComponent to Form page

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-indent */
 import React from 'react';
-import FormComponent from '../components/FormComponent';
 import ContactComponent from '../components/ContactComponent';
+import FormComponent from '../components/FormComponent';
 
 export default function Form() {
   return (
