5fa25edd9c49589d6f1eb0f6ccbf2d03295a5fc2
PIERRE DARTEVELLE <pdarteve5@gmail.com>
2022-04-21 15:19:07 +0200

    added carousel to fit figma

--- src/components/HomeBookSelection.jsx
+++ src/components/HomeBookSelection.jsx
@@ -1,18 +1,21 @@
 /* eslint-disable react/jsx-indent */
 import bookList from '../ressources/livresDB.json';
+import Vintage from '../assets/vintage.jpg';
 
 function HomeBookSelection() {
   return (
     <div className="wrapper">
       <h2>Sélection du mois :</h2>
       <div className="bookSelection">
-        <div className="carousel">
+        <div className="container">
           {bookList.slice(0, 10).map((book) => (
-            <p>
+            <p className="carouselItems">
               {' '}
               {book.note === 5 ? (
                 <img src={book.picture} alt="preview" />
-              ) : null}
+              ) : (
+                <img src={Vintage} alt="default cover" />
+              )}
             </p>
           ))}
         </div>
