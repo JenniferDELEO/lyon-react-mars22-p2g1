292172312d2f13cc6c2e4021d5bebe20fb25c402
01adrien <adrien.go@outlook.fr>
2022-04-24 18:33:15 +0200

    fixed issues related to api

--- src/components/ratingStar.jsx
+++ src/components/ratingStar.jsx
@@ -1,16 +1,20 @@
-export default function RatingStar({ rate, id }) {
-  console.log(typeof id);
+export default function RatingStar({ rate }) {
   const yellowStar = new Array(rate).fill('');
   const greyStar = new Array(5 - rate).fill('');
+  function uniqId() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
   return (
     <div className="text-xl pb-5 flex">
       {yellowStar.map(() => (
-        <p key={id} className="text-yellow-400">
+        <p key={uniqId()} className="text-yellow-400">
           ★
         </p>
       ))}
       {greyStar.map(() => (
-        <p key={id} className="text-slate-400">
+        <p key={uniqId()} className="text-slate-400">
           ★
         </p>
       ))}
