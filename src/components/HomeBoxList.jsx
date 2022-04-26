08855649e55f6d1d7938f78dec89cd88bbd052eb
PIERRE DARTEVELLE <pdarteve5@gmail.com>
2022-04-26 17:34:41 +0200

    box selection is now linked to DB

--- src/components/HomeBoxList.jsx
+++ src/components/HomeBoxList.jsx
@@ -1,31 +1,42 @@
-import boxList from '../ressources/coordsBAL.json';
 import bookList from '../ressources/livresDB.json';
 import { Link } from 'react-router-dom';
 import '../styles/HomeLists.css';
 import BookBox from '../assets/box_middlefull.png';
-
-const quantiteParBoite = [];
-
-for (let i = 0; i < boxList.length; i += 1) {
-  quantiteParBoite.push(0);
-}
-for (let i = 0; i < bookList.length; i += 1) {
-  const index = parseFloat(bookList[i].boite);
-  quantiteParBoite[index] += 1;
-}
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function HomeBoxList() {
+  const [boxList, setBoxList] = useState([]);
+  const quantiteParBoite = [];
+
+  for (let i = 0; i < boxList.length; i += 1) {
+    quantiteParBoite.push(0);
+  }
+  for (let i = 0; i < bookList.length; i += 1) {
+    const index = parseFloat(bookList[i].boite);
+    quantiteParBoite[index] += 1;
+  }
+  useEffect(() => {
+    axios
+      .get('http://localhost:4000/boxes')
+      .then((result) => result.data)
+      .then((result) => {
+        setBoxList(result);
+        console.log(result);
+      });
+  }, []);
+
   return (
     <div className="boxList">
       <h2>Liste des boîtes : </h2>
       <ul>
         {boxList.slice(0, 3).map((box) => (
-          <Link to={`/BoxDetail/${box.boite}`}>
-            <p key={box.boite}>
+          <Link to={`/BoxDetail/${box.id}`}>
+            <p key={box.id}>
               {' '}
               <img src={BookBox} alt="boite a livre" /> {box.adresse}, {box.CP}
               <br />
-              {quantiteParBoite[box.boite]} livre(s)
+              {quantiteParBoite[box.id]} livre(s)
             </p>
           </Link>
         ))}
