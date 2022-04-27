e5ff0b3b866311649ec59867da36cb923974affc
Jennifer DE LEO <deleo.jennifer1@gmail.com>
2022-04-27 17:31:17 +0200

    started carousel homepage

--- src/components/HomeBookSelection.jsx
+++ src/components/HomeBookSelection.jsx
@@ -1,24 +1,39 @@
-/* eslint-disable react/jsx-indent */
 import bookList from '../ressources/livresDB.json';
-import Vintage from '../assets/vintage.jpg';
+// import Vintage from '../assets/vintage.jpg';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import '../styles/HomeLists.css';
 
 function HomeBookSelection() {
+  const bookNote = bookList.filter((book) => book.note >= 4);
+  const bookPicture = bookNote.filter((book) => book.picture);
+
   return (
     <div className="wrapper">
       <h2>Sélection du mois :</h2>
-      <div className="bookSelection">
-        <div className="carouselContainer">
-          {bookList.slice(0, 10).map((book) => (
-            <p className="carouselItems" key={book.ISBN}>
+      <div className="carouselContainer">
+        <Carousel
+          showStatus={false}
+          infiniteLoop
+          // emulateTouch
+          // autoPlay
+          useKeyboardArrows
+          transitionTime={1000}
+          // axis="vertical"
+          selectedItem={0}
+        >
+          {bookPicture.map((book) => (
+            <div className="carouselItems" key={book.ISBN}>
               {' '}
-              {book.note === 5 ? (
-                <img src={book.picture} alt="preview" />
-              ) : (
-                <img src={Vintage} alt="default cover" />
-              )}
-            </p>
+              <img src={book.picture} alt="preview" />
+              <p className="legend">
+                {book.titre}
+                {' de '}
+                {book.auteur}
+              </p>
+            </div>
           ))}
-        </div>
+        </Carousel>
       </div>
     </div>
   );
