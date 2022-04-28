84d4162a012a0b444927745ec2b66358b1907e34
PIERRE DARTEVELLE <pdarteve5@gmail.com>
2022-04-28 11:39:58 +0200

    cleaned up the code

--- src/components/HomeBoxList.jsx
+++ src/components/HomeBoxList.jsx
@@ -1,11 +1,9 @@
-/* eslint-disable react/jsx-one-expression-per-line */
 import { Link } from 'react-router-dom';
 import '../styles/HomeLists.css';
 import BookBox from '../assets/box_middlefull.png';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
-// On déclare des states pour nos tables de livres et de boites
 function HomeBoxList({ CP }) {
   const [boxList, setBoxList] = useState([]);
   const [selectedBox, setSelectedBox] = useState('');
@@ -33,9 +31,13 @@ function HomeBoxList({ CP }) {
             key={box.id}
           >
             <img src={BookBox} alt="boite a livre" />
-            {box.adresse}, {box.CP}
+            {box.adresse}
+            ,
+            {box.CP}
             <br />
-            {box.quantity} livre(s)
+            {box.quantity}
+            {' '}
+            livre(s)
           </p>
         </Link>
       ))}
