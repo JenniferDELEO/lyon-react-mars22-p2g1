4773e6d4666b1b23659a947a0c513927727f02c0
Jennifer DE LEO <deleo.jennifer1@gmail.com>
2022-04-29 12:40:37 +0200

    css for the selection of the month

--- src/components/HomeBookSelection.jsx
+++ src/components/HomeBookSelection.jsx
@@ -1,8 +1,9 @@
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import { Carousel } from 'react-responsive-carousel';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 import '../styles/HomeLists.css';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function HomeBookSelection() {
   const [getBook, setGetBook] = useState([]);
@@ -16,6 +17,7 @@ function HomeBookSelection() {
           data
             .filter((book) => book.note >= 4)
             .filter((book) => book.picture)
+            .sort(() => 0.5 - Math.random())
             .slice(0, 10)
         );
       })
@@ -28,26 +30,34 @@ function HomeBookSelection() {
     <div className="wrapper">
       <h2>Sélection du mois :</h2>
       <div className="carouselContainer">
-        <Carousel
-          showStatus={false}
-          infiniteLoop
+        <AliceCarousel
           autoPlay
-          useKeyboardArrows
-          transitionTime={1000}
-          selectedItem={0}
-        >
-          {getBook.map((book) => (
-            <div className="carouselItems" key={book.isbn}>
-              {' '}
-              <img src={book.picture} alt="preview" />
-              <p className="legend">
-                {book.title}
-                {' de '}
-                {book.author}
-              </p>
-            </div>
+          autoPlayInterval={2000}
+          infinite
+          responsive={{
+            0: {
+              items: 1,
+            },
+            200: {
+              items: 3,
+            },
+          }}
+          items={getBook.map((book) => (
+            <Link to={`/bookdetail/${book.id}`}>
+              <div className="carouselHomepage">
+                <div className="carouselItems" key={book.isbn}>
+                  {' '}
+                  <img src={book.picture} alt="preview" role="presentation" />
+                </div>
+                <p className="legend">
+                  {book.title}
+                  {' de '}
+                  {book.author}
+                </p>
+              </div>
+            </Link>
           ))}
-        </Carousel>
+        />
       </div>
     </div>
   );
