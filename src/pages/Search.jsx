bb9046052d8765d0f76e12c886412d969d89a1d4
PIERRE DARTEVELLE <pdarteve5@gmail.com>
2022-05-04 17:27:51 +0200

    switch to only one button for both search types

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,25 +1,16 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import '../styles/Search.css';
+import { Link } from 'react-router-dom';
 
 export default function Search() {
   const [userEntry, setUserEntry] = useState('');
   const [searchResult, setSearchResult] = useState([]);
   const handleChange = (e) => setUserEntry(e.target.value);
 
-  function getTitle() {
+  function getSearch() {
     axios
-      .get(`${process.env.REACT_APP_API_URL}/books/search?title=${userEntry}`)
-      .then((result) => result.data)
-      .then((result) => {
-        console.log(result);
-        setSearchResult(result);
-      });
-    setUserEntry('');
-  }
-  function getAuthor() {
-    axios
-      .get(`${process.env.REACT_APP_API_URL}/books/search?author=${userEntry}`)
+      .get(`${process.env.REACT_APP_API_URL}books/search?search=${userEntry}`)
       .then((result) => result.data)
       .then((result) => {
         console.log(result);
@@ -38,28 +29,29 @@ export default function Search() {
           onChange={handleChange}
         />
         <div className="ButtonPortion">
-          <button className="Btn SearchTitle" type="button" onClick={getTitle}>
+          <button className="Btn SearchBtn" type="button" onClick={getSearch}>
             Search by Title
-          </button>
-          <button
-            className="Btn SearchAuthor"
-            type="button"
-            onClick={getAuthor}
-          >
-            Search by Author
           </button>
         </div>
       </div>
       <div className="PreviewPortion">
         {searchResult.map((book) => (
-          <img
-            src={
-              book.picture === null || book.picture === 'None'
-                ? 'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/vintage-book-cover-template-design-46e27bb5bb18d1354f5acc1d96454f60_screen.jpg?ts=1637015775'
-                : book.picture
-            }
-            alt={book.title}
-          />
+          <div className="Card">
+            {' '}
+            <Link to={`/bookdetail/${book.id}`}>
+              {' '}
+              <img
+                src={
+                  book.picture === null || book.picture === 'None'
+                    ? 'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/vintage-book-cover-template-design-46e27bb5bb18d1354f5acc1d96454f60_screen.jpg?ts=1637015775'
+                    : book.picture
+                }
+                alt={book.title}
+              />
+            </Link>
+            <h2>{book.title}</h2>
+            <h3>{book.author}</h3>
+          </div>
         ))}
       </div>
     </div>
