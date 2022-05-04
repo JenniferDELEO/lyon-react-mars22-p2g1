497a548bce76339458e0dbd8b68fd6129b4423fd
PIERRE DARTEVELLE <pdarteve5@gmail.com>
2022-05-05 00:11:03 +0200

    added a holding list to fill up the screen before research, started styling a bit

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,58 +1,84 @@
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/Search.css';
 import { Link } from 'react-router-dom';
+import Vintage from '../assets/vintage.jpg';
 
 export default function Search() {
   const [userEntry, setUserEntry] = useState('');
   const [searchResult, setSearchResult] = useState([]);
+  const [getBooks, setGetBooks] = useState([]);
   const handleChange = (e) => setUserEntry(e.target.value);
 
-  function getSearch() {
+  function handleSearch() {
     axios
       .get(`${process.env.REACT_APP_API_URL}books/search?search=${userEntry}`)
       .then((result) => result.data)
       .then((result) => {
-        console.log(result);
         setSearchResult(result);
       });
     setUserEntry('');
   }
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}books/note/5`)
+      .then((result) => result.data)
+      .then((result) => {
+        setGetBooks(result[0]);
+      });
+  }, []);
+
   return (
     <div className="SearchWrapper">
       <div className="SearchPortion">
         <input
           className="SearchInput"
           type="text"
-          placeholder={'Recherche par titre'}
+          placeholder={'Entrez votre recherche ici'}
           value={userEntry}
           onChange={handleChange}
         />
         <div className="ButtonPortion">
-          <button className="Btn SearchBtn" type="button" onClick={getSearch}>
-            Search by Title
+          <button
+            className="Btn SearchBtn"
+            type="button"
+            onClick={handleSearch}
+          >
+            Lancez votre recherche
           </button>
         </div>
       </div>
       <div className="PreviewPortion">
         {searchResult.map((book) => (
-          <div className="Card">
+          <div className="Card" key={book.ISBN}>
             {' '}
             <Link to={`/bookdetail/${book.id}`}>
               {' '}
               <img
                 src={
                   book.picture === null || book.picture === 'None'
-                    ? 'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/vintage-book-cover-template-design-46e27bb5bb18d1354f5acc1d96454f60_screen.jpg?ts=1637015775'
+                    ? { Vintage }
                     : book.picture
                 }
                 alt={book.title}
               />
             </Link>
-            <h2>{book.title}</h2>
-            <h3>{book.author}</h3>
+            <div className="Bookinfo">
+              <h3>{book.title}</h3>
+              <h4>{book.author}</h4>
+            </div>
           </div>
         ))}
+        <div className="listholder">
+          {getBooks.map((book) => (
+            <div className="Card listHolderItem" key={book.ISBN}>
+              {' '}
+              <img src={book.picture} alt={book.title} />
+              <h3>{book.title}</h3>
+              <h4>{book.author}</h4>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
