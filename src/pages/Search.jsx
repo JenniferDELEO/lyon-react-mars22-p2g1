dc251a6bd87f838d52e0a72844d1a5f0dae68ec9
PIERRE DARTEVELLE <pdarteve5@gmail.com>
2022-05-04 08:09:20 +0200

    initial commit

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,44 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 
 export default function Search() {
-  return <div>Search page</div>;
+  const [titleEntered, setTitleEntered] = useState('');
+
+  const handleFilter = (event) => {
+    setTitleEntered(event.target.value);
+  };
+
+  //   const getTitle = () => {
+  //       axios
+  //         .get(`${process.env.REACT_APP_API_URL}books/search${titleEntered}`)
+  //         .then((result) => result.data[0])
+  //         .then((result) => {
+  // console.log(result)
+  //         });
+  //     };
+  //     console.log(titleEntered);
+  //   };
+  function getTitle() {
+    axios
+      .get(
+        `${process.env.REACT_APP_API_URL}/books/search?title=${titleEntered}`
+      )
+      .then((result) => result.data[0])
+      .then((result) => {
+        console.log(result);
+      });
+  }
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder={'Lancez une recherche ici'}
+        value={titleEntered}
+        onChange={handleFilter}
+      />
+      <button type="button" onClick={getTitle}>
+        Title search
+      </button>
+    </div>
+  );
 }
