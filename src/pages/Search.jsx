d2707d1e708c6e6b641f06624dd29bad0810adfb
PIERRE DARTEVELLE <pdarteve5@gmail.com>
2022-05-04 16:02:37 +0200

    user can search via author or title, not much style yet

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,32 +1,67 @@
 import axios from 'axios';
 import React, { useState } from 'react';
+import '../styles/Search.css';
 
 export default function Search() {
-  const [titleEntered, setTitleEntered] = useState('');
-
-  const handleFilter = (event) => {
-    setTitleEntered(event.target.value);
-  };
+  const [userEntry, setUserEntry] = useState('');
+  const [searchResult, setSearchResult] = useState([]);
+  const handleChange = (e) => setUserEntry(e.target.value);
 
   function getTitle() {
     axios
-      .get(`${process.env.REACT_APP_API_URL}books/search?title=${titleEntered}`)
-      .then((result) => result.data[0])
+      .get(`${process.env.REACT_APP_API_URL}/books/search?title=${userEntry}`)
+      .then((result) => result.data)
       .then((result) => {
         console.log(result);
+        setSearchResult(result);
       });
+    setUserEntry('');
+  }
+  function getAuthor() {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/books/search?author=${userEntry}`)
+      .then((result) => result.data)
+      .then((result) => {
+        console.log(result);
+        setSearchResult(result);
+      });
+    setUserEntry('');
   }
   return (
-    <div>
-      <input
-        type="text"
-        placeholder={'Lancez une recherche ici'}
-        value={titleEntered}
-        onChange={handleFilter}
-      />
-      <button type="button" onClick={getTitle}>
-        Search by Title
-      </button>
+    <div className="SearchWrapper">
+      <div className="SearchPortion">
+        <input
+          className="SearchInput"
+          type="text"
+          placeholder={'Recherche par titre'}
+          value={userEntry}
+          onChange={handleChange}
+        />
+        <div className="ButtonPortion">
+          <button className="Btn SearchTitle" type="button" onClick={getTitle}>
+            Search by Title
+          </button>
+          <button
+            className="Btn SearchAuthor"
+            type="button"
+            onClick={getAuthor}
+          >
+            Search by Author
+          </button>
+        </div>
+      </div>
+      <div className="PreviewPortion">
+        {searchResult.map((book) => (
+          <img
+            src={
+              book.picture === null || book.picture === 'None'
+                ? 'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/vintage-book-cover-template-design-46e27bb5bb18d1354f5acc1d96454f60_screen.jpg?ts=1637015775'
+                : book.picture
+            }
+            alt={book.title}
+          />
+        ))}
+      </div>
     </div>
   );
 }
