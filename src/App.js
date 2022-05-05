3d1fc1429a982613427fdd68d135add5b4e7ae6c
Jennifer DE LEO <deleo.jennifer1@gmail.com>
2022-05-05 10:29:32 +0200

    started new navbar + global style desktop

--- src/App.js
+++ src/App.js
@@ -4,35 +4,47 @@ import Main from './components/Main';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
 import React, { useEffect, useState } from 'react';
+import useScroll from './hooks/useScroll';
 
 function App() {
-  const [show, setShow] = useState(true);
-  const controlNavbar = () => {
-    if (document.documentElement.scrollTop > 80 && window.innerWidth < 650) {
-      setShow(false);
-    } else setShow(true);
-  };
+  const searchBoxHeight = 50;
+  const [height, setHeight] = useState(searchBoxHeight);
+  const scroll = useScroll({
+    wait: 50,
+  });
 
   useEffect(() => {
-    window.addEventListener('scroll', controlNavbar);
-    return () => {
-      window.removeEventListener('scroll', controlNavbar);
-    };
-  }, []);
+    if (scroll.direction === 'up') {
+      setHeight((prevHeight) => {
+        const newHeight = prevHeight - scroll.deltaY;
+        return newHeight > searchBoxHeight ? searchBoxHeight : newHeight;
+      });
+    } else if (scroll.direction === 'down') {
+      setHeight((prevHeight) => {
+        const leftToScroll =
+          document.documentElement.scrollHeight - scroll.y - window.innerHeight;
+        if (leftToScroll <= searchBoxHeight) setHeight(searchBoxHeight);
+
+        const newHeight = prevHeight - scroll.deltaY;
+        return newHeight < 0 ? 0 : newHeight;
+      });
+    }
+  }, [scroll]);
 
   return (
     <div className="app-container">
       <div className="header">
         <Header />
       </div>
-      <div className={`navbar-fixed ${show && 'navbar'}`}>
-        <NavBar />
-      </div>
+
       <div className="main">
         <Main />
       </div>
       <div className="footer">
         <Footer />
+      </div>
+      <div id="searchbar" className={'navbar-fixed'} style={{ height }}>
+        <NavBar />
       </div>
     </div>
   );
