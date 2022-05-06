f6769da6400bd7621bb4e1723d63c807f5791858
PIERRE DARTEVELLE <pdarteve5@gmail.com>
2022-05-07 00:23:31 +0200

    adapted search to launch upon user input instead of button click

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-return-assign */
 import axios from 'axios';
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/Search.css';
 import { Link } from 'react-router-dom';
 import RatingStar from '../components/ratingStar';
@@ -11,22 +11,22 @@ export default function Search() {
   const [getBooks, setGetBooks] = useState([]);
   const [userHasSearched, setUserHasSearched] = useState(false);
 
-  const form = useRef();
   const handleChange = (e) => {
     setUserEntry(e.target.value);
   };
 
-  function handleSearch(e) {
-    e.preventDefault();
-    axios
-      .get(`${process.env.REACT_APP_API_URL}books/search?search=${userEntry}`)
-      .then((result) => result.data)
-      .then((result) => {
-        setSearchResult(result);
-        setUserHasSearched(true);
-      });
-    setUserEntry('');
-  }
+  useEffect(() => {
+    if (userEntry) {
+      axios
+        .get(`${process.env.REACT_APP_API_URL}books/search?search=${userEntry}`)
+        .then((result) => result.data)
+        .then((result) => {
+          setSearchResult(result);
+          setUserHasSearched(true);
+        });
+    }
+  }, [handleChange]);
+
   useEffect(() => {
     axios
       .get(`${process.env.REACT_APP_API_URL}books/note/5`)
@@ -39,31 +39,22 @@ export default function Search() {
   return (
     <div className="SearchWrapper">
       <div className="SearchPortion">
-        <form onSubmit={handleSearch} ref={form}>
-          <input
-            className="SearchInput"
-            type="text"
-            placeholder={'Entrez votre recherche ici'}
-            value={userEntry}
-            onChange={handleChange}
-          />
-          <button className="Btn SearchBtn" type="submit">
-            Lancez votre recherche
-          </button>
-        </form>
+        <input
+          className="SearchInput"
+          type="text"
+          placeholder={'Entrez votre recherche ici'}
+          value={userEntry}
+          onChange={handleChange}
+        />
+        {/* <button className="Btn SearchBtn" type="submit">
+          Lancez votre recherche
+        </button> */}
       </div>
       <div className="PreviewPortion">
         {searchResult.map((book) => (
           <div className="Card" key={book.ISBN}>
             {' '}
-            <img
-              src={
-                !book.picture.startsWith('http')
-                  ? 'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/vintage-book-cover-template-design-46e27bb5bb18d1354f5acc1d96454f60_screen.jpg?ts=1637015775'
-                  : book.picture
-              }
-              alt={book.title}
-            />
+            <img src={book.picture} alt={book.title} />
             <div className="book-infos">
               <div>
                 <Link to={`/bookdetail/${book.id}`}>
